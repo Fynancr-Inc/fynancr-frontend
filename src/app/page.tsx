c432012aaef53690f6cf658025371daa38be6a00
Nitin Gupta <nitin@fynancr.com>
2025-07-25 11:06:07 +0530

    Initial Set up

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white text-black text-3xl font-semibold">
+      Welcome to Fynancr
+    </div>
+  );
+}
